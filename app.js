bf66b459444d933ab8cb192990b0719e3aed886a
Julia Veras <jujubaveras@outlook.com>
2023-09-15 13:25:41 -0300

    fix problem with text that wasn't changing with js

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const colorSpan = document.querySelector('.color')
 btn.addEventListener('click', function () {
   const randomNumber = Math.floor(Math.random() * colors.length)
   document.body.style.backgroundColor = colors[randomNumber]
-  color.textContent = colors[randomNumber]
+  colorSpan.textContent = colors[randomNumber]
 })
